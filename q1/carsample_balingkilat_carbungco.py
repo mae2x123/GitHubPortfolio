e644a5d7cbd5b8be493e59bf94dd61e7380e9fc5
mae2x123 <mpcarbungco2030@clc.pshs.edu.ph>
2026-09-17 10:20:18 +0800

    Add files via upload

---
+++ q1/carsample_balingkilat_carbungco.py
@@ -0,0 +1,29 @@
+#Car Game
+class Car:
+    def __init__(self,brand,model,battery=33):
+        self.brand=brand
+        self.model=model
+        self.battery=battery
+    def go(self,distance):
+        self.battery -= distance/25
+        print("You travelled",distance,"km")
+        print("Your",self.brand,self.model,"has",self.battery,"wH left")
+    def charge(self,wH):
+        self.battery +=wH
+        print("You charged your car")
+        print("Your",self.brand,self.model,"has",self.battery,"wH left")
+
+brand=input("What is the brand of your car? ")
+model=input("What is th emodel of your car? ")
+myCar=Car(brand,model)
+while myCar.battery>0:
+    action=input("What do you want to do? (go, charge) ")
+    if action == "go":
+        distance=int(input("How far? "))
+        myCar.go(distance)
+    elif action == "charge":
+        wH=int(input("How much to charge?"))
+        myCar.charge(wH)
+    else:
+        print("Invalid action")
+print("Your car ran out of battery.")
